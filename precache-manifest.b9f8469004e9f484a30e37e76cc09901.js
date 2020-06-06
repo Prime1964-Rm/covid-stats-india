8b5b3ebac5f9b9ece7be181a38970dd35888abd6
Prime1964 <rishabh.mishra191997@gmail.com>
2020-06-07 03:59:08 +0530

    Updates

---
+++ precache-manifest.b9f8469004e9f484a30e37e76cc09901.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6cd7e0da4d9dc52ae281b112bea562a4",
+    "url": "/covid-stats-india/index.html"
+  },
+  {
+    "revision": "3fcacec68db1ee471f39",
+    "url": "/covid-stats-india/static/css/main.53ffee78.chunk.css"
+  },
+  {
+    "revision": "fe3b186291b1d02f8f9b",
+    "url": "/covid-stats-india/static/js/2.14ba0e2c.chunk.js"
+  },
+  {
+    "revision": "b1161d377c873a4659fa05e6fe5a6971",
+    "url": "/covid-stats-india/static/js/2.14ba0e2c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3fcacec68db1ee471f39",
+    "url": "/covid-stats-india/static/js/main.ea850c7e.chunk.js"
+  },
+  {
+    "revision": "e9a4868bb95565b7e798",
+    "url": "/covid-stats-india/static/js/runtime-main.0a51cd73.js"
+  }
+]);
